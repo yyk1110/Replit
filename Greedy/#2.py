c06ace1544efc10d6c5a9731fa42aabcf1def249
yyk1110 <127828417+yyk1110@users.noreply.github.com>
2025-01-30 18:40:34 +0000

    example: 큰 수의 법칙

---
+++ Greedy/#2.py
@@ -0,0 +1,20 @@
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+result = 0
+
+data.sort() #입력받은 수 정렬
+first = data[N-1]
+second = data[N-2]
+
+while True:
+  for i in range(K): #가장 큰 수 K번 더하기
+    if M == 0:
+      break
+    result += first
+    M -= 1 #더할 때마다 1 빼기
+  #if M == 0:
+  #  break
+  result += second
+  M -= 1
+
+print(result)
