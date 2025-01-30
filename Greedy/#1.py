323c2a0b182a689aaa120ef37ae041daa11bbd61
yyk1110 <127828417+yyk1110@users.noreply.github.com>
2025-01-30 18:08:04 +0000

    example: 거스름돈

--- Greedy/#1.py
+++ Greedy/#1.py
@@ -1,2 +1,10 @@
-for i in range(5):
-  print('A number:', i)
+n = 1260
+count = 0
+
+coin_types = [500, 100, 50, 10] #큰 단위 화폐부터 선언
+
+for coin in coin_types:
+  count += n // coin #해당 화폐로 거슬러 줄 수 있는 동전 개수
+  n %= coin #거슬러 준 후 잔액
+
+print(count)
