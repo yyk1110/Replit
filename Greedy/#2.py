505d0a9e69d8924f16b2d9024751645fa9e969fe
YeoKyoung Yoon <127828417+yyk1110@users.noreply.github.com>
2025-02-06 07:25:27 +0000

    example: 큰 수의 법칙

--- Greedy/#2.py
+++ Greedy/#2.py
@@ -1,3 +1,24 @@
+print("Version 2")
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[N-1]
+second = data[N-2]
+
+count = (M // (K+1)) * K #가장 큰 수가 더해질 횟수
+count += M % (K+1)
+
+result = 0
+result += (count) * first #가장 큰 수 * 반복 가능한 수
+result += (M - count) * second #두 번째로 큰 수 더함
+
+print(result)
+
+
+
+'''
+print('Version.1')
 N, M, K = map(int, input().split())
 data = list(map(int, input().split()))
 result = 0
@@ -18,3 +39,4 @@ while True:
   M -= 1
 
 print(result)
+'''
